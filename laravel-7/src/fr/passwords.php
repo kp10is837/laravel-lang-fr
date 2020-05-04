1caba1427ff43bc5c0b87c47794fc702db2bc9ac
stephweb <contact@devandweb.fr>
2020-05-04 13:25:18 +0200

    Laravel 7

--- laravel-7/src/fr/passwords.php
+++ laravel-7/src/fr/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    "password" => "Les mots de passe doivent avoir au moins six caractères et doivent être identiques.",
-    "reset" => "Votre mot de passe a été réinitialisé.",
-    "sent" => "Nous vous avons envoyé par courriel le lien de réinitialisation du mot de passe.",
+    "reset" => "Votre mot de passe a été réinitialisé !",
+    "sent" => "Nous vous avons envoyé par courriel le lien de réinitialisation du mot de passe !",
+    'throttled' => 'Veuillez patienter avant de réessayer.',
     "token" => "Ce jeton de réinitialisation du mot de passe n'est pas valide.",
-    "user" => "Aucun utilisateur n'a été trouvé avec cette adresse mail.",
+    "user" => "Aucun utilisateur n'a été trouvé avec cette adresse e-mail.",
 
 ];
